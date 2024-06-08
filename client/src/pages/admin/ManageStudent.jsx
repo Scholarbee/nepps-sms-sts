9bfbfd681995ea1131d0a864a8d08571db9d46c1
Scholarbee <scholarbee6@gmail.com>
2024-06-08 20:43:25 +0000

    manage student draft

--- client/src/pages/admin/ManageStudent.jsx
+++ client/src/pages/admin/ManageStudent.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "../../components/global/Navbar";
-import { Box } from "@mui/material";
+import { Box, Button, TextField, InputAdornment, Grid } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import AddIcon from "@mui/icons-material/Add";
 
 function ManageStudent() {
   return (
@@ -18,6 +20,48 @@ function ManageStudent() {
         >
           <h1>Manage Student</h1>
         </Box>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={8}>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent:{ xs: "space-between", sm: "space-around" },
+                alignItems: "center",
+                // gap: "10px",
+                padding: "0 20px",
+              }}
+            >
+              <Box>
+                <TextField
+                  type="search"
+                  variant="outlined"
+                  placeholder="Search student"
+                  // sx={{ width: { xs: "100%", sm: "300px" } }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SearchIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Box>
+              <Box>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  startIcon={<AddIcon />}
+                  sx={{ width: { xs: "100%", sm: "auto" } }}
+                >
+                  Add
+                </Button>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            Class Stats
+          </Grid>
+        </Grid>
       </section>
     </>
   );
